339bc5997ee6a0ac7da45fb7519dffd9ad9d5c40
dennisleenders <dennisleendersmv@gmail.com>
2014-11-11 17:07:20 +0100

    added loader html

--- living-web/public/javascripts/app.js
+++ living-web/public/javascripts/app.js
@@ -39650,60 +39650,60 @@ function stopScroll(){
 }
 
 
-var controls = new function () {
-    this.cameraNear = camera.near;
-    this.cameraFar = camera.far;
-    this.cameraX = camera.position.x;
-    this.cameraZ = camera.position.z;
-    this.cameraY = camera.position.y;
+// var controls = new function () {
+//     this.cameraNear = camera.near;
+//     this.cameraFar = camera.far;
+//     this.cameraX = camera.position.x;
+//     this.cameraZ = camera.position.z;
+//     this.cameraY = camera.position.y;
 
-    this.removeCube = function () {
-        var allChildren = scene.children;
-        var lastObject = allChildren[allChildren.length - 1];
-        if (lastObject instanceof THREE.Mesh) {
-            scene.remove(lastObject);
-        }
-    }
+//     this.removeCube = function () {
+//         var allChildren = scene.children;
+//         var lastObject = allChildren[allChildren.length - 1];
+//         if (lastObject instanceof THREE.Mesh) {
+//             scene.remove(lastObject);
+//         }
+//     }
 
-    this.addTriangle = function () {
-        var cubeSize = Math.random();
-        var cubeGeometry = new THREE.TetrahedronGeometry(cubeSize,0);
-        var cubeMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff });
-        var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        cube.castShadow = true;
+//     this.addTriangle = function () {
+//         var cubeSize = Math.random();
+//         var cubeGeometry = new THREE.TetrahedronGeometry(cubeSize,0);
+//         var cubeMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff });
+//         var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+//         cube.castShadow = true;
 
-        // position the cube randomly in the scene
-        cube.position.x = -60 + Math.round((Math.random() * 100));
-        cube.position.y = Math.round((Math.random() * 10));
-        cube.position.z = -100 + Math.round((Math.random() * 150));
+//         // position the cube randomly in the scene
+//         cube.position.x = -60 + Math.round((Math.random() * 100));
+//         cube.position.y = Math.round((Math.random() * 10));
+//         cube.position.z = -100 + Math.round((Math.random() * 150));
 
-        // add the cube to the scene
-        scene.add(cube);
-    };
+//         // add the cube to the scene
+//         scene.add(cube);
+//     };
 
-    this.outputObjects = function () {
-        console.log(scene.children);
-    }
-}
+//     this.outputObjects = function () {
+//         console.log(scene.children);
+//     }
+// }
 
-var gui = new dat.GUI();
-gui.add(controls, 'addTriangle');
-gui.add(controls, 'removeCube');
-gui.add(controls, 'cameraNear', 0, 50).onChange(function (e) {
-    camera.near = e;
-});
-gui.add(controls, 'cameraFar', 50, 200).onChange(function (e) {
-    camera.far = e;
-});
-gui.add(controls, 'cameraX', 0, 200).onChange(function (e) {
-    camera.position.x = e;
-});
-gui.add(controls, 'cameraY', 0, 200).onChange(function (e) {
-    camera.position.y = e;
-});
-gui.add(controls, 'cameraZ', 0, 200).onChange(function (e) {
-    camera.position.z = e;
-});
+// var gui = new dat.GUI();
+// gui.add(controls, 'addTriangle');
+// gui.add(controls, 'removeCube');
+// gui.add(controls, 'cameraNear', 0, 50).onChange(function (e) {
+//     camera.near = e;
+// });
+// gui.add(controls, 'cameraFar', 50, 200).onChange(function (e) {
+//     camera.far = e;
+// });
+// gui.add(controls, 'cameraX', 0, 200).onChange(function (e) {
+//     camera.position.x = e;
+// });
+// gui.add(controls, 'cameraY', 0, 200).onChange(function (e) {
+//     camera.position.y = e;
+// });
+// gui.add(controls, 'cameraZ', 0, 200).onChange(function (e) {
+//     camera.position.z = e;
+// });
 
 
 
