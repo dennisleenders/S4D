c6dbada2750522d4c82de095d0ea9f84c69a6487
dennisleenders <dennisleendersmv@gmail.com>
2014-11-11 13:49:13 +0100

    added scrollinput

---
+++ examples/scroll-input/public-source/javascripts/constructor/global-variables.js
@@ -0,0 +1,15 @@
+
+// All global variables we will be using
+var speed;
+var step = 0;
+var rotationSpeed = 0.01;
+var directionHeartUp = true;
+var directionHeartDown = false;
+var isScroll = false;
+var scrollSpeed;
+var checkScroll;
+
+// Will hold all the multiple material objects for click events
+// WE only need this because we have multiple material objects, else we could use
+// scene.childerns array
+var objects = [];
