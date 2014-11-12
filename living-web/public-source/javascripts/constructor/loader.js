70652d4a1ceb42517cc6fe14daa4f72923ad66a9
dennisleenders <dennisleendersmv@gmail.com>
2014-11-12 17:48:49 +0100

    removing comments

--- living-web/public-source/javascripts/constructor/loader.js
+++ living-web/public-source/javascripts/constructor/loader.js
@@ -2,5 +2,4 @@
 // setting the timeout for the loading screen
 setTimeout(function() {
   $(".loader").fadeOut(2000,"swing");
-  console.log("fade");
 }, 8000);
--- living-web/public/javascripts/app.js
+++ living-web/public/javascripts/app.js
@@ -39255,7 +39255,6 @@ peopleWhoDontUseChromeDetector();
 // setting the timeout for the loading screen
 setTimeout(function() {
   $(".loader").fadeOut(2000,"swing");
-  console.log("fade");
 }, 8000);
 
 
